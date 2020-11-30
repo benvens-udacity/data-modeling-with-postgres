7d59d7927a1c3d0a6acd942f50ce67809e0f6155
Simon Benvenutto <simon.benvenutto@gmail.com>
2020-11-30 05:17:20 +0000

    add local bridge network

--- run-postgres.sh
+++ run-postgres.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-  docker run --name local-postgres -p 5432:5432 -v /home/simonb/Services/POSTGRES:/var/lib/postgresql/data -e POSTGRES_PASSWORD=mysecretpassword -d postgres:latest
+  docker run --name local-postgres --network udacity-net --publish 5432:5432 -v /home/simonb/Services/POSTGRES:/var/lib/postgresql/data -e POSTGRES_PASSWORD=mysecretpassword -d postgres:latest
