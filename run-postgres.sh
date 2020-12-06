79ac259061e9b218d97c1e060dbbf36cb0cf20ec
Simon Benvenutto <simon.benvenutto@gmail.com>
2020-12-06 15:53:49 +0000

    typo

--- run-postgres.sh
+++ run-postgres.sh
@@ -4,4 +4,3 @@
   docker run --name local-postgres --network udacity-net --publish 5432:5432 \
          -v /home/simonb/Services/POSTGRES:/var/lib/postgresql/data -e POSTGRES_USER=student \
          -e POSTGRES_PASSWORD=student -d postgres:latest
-         ß
