65b2b0e05328702f20f56fb5e4a4a5048eeb618a
Simon Benvenutto <simon.benvenutto@gmail.com>
2020-12-06 18:08:06 +0000

    add default DB name

--- run-postgres.sh
+++ run-postgres.sh
@@ -3,4 +3,4 @@
   docker network create udacity-net
   docker run --name local-postgres --network udacity-net --publish 5432:5432 \
          -v /home/simonb/Services/POSTGRES:/var/lib/postgresql/data -e POSTGRES_USER=student \
-         -e POSTGRES_PASSWORD=student -d postgres:latest
+         -e POSTGRES_PASSWORD=student -e POSTGRES_DB=studentdb -d postgres:latest
