d89301f30693bef5667aed6ccd5d1aae569ac090
Simon Benvenutto <simon.benvenutto@gmail.com>
2021-03-06 18:39:42 +0000

    in users table, change updation to update subcription level (e.g.: user upgrades from free to paid plan)

--- sql_queries.py
+++ sql_queries.py
@@ -85,7 +85,8 @@ user_table_insert = ("""INSERT INTO users (
                         ) VALUES(
                             %s, %s, %s, %s, %s
                         )
-                        ON CONFLICT DO NOTHING
+                        ON CONFLICT (user_id) DO UPDATE 
+                        SET level = excluded.level
                         ;""")
 
 song_table_insert = ("""INSERT INTO songs (
